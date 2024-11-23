0b8189f807afb7c9a37169c82175f9113ea919c5
cabdishakurr <cabdishakurrrr@gmail.com>
2024-11-23 14:18:30 +0300

    e
    dd

--- models/hall_invoice.py
+++ models/hall_invoice.py
@@ -1,4 +1,5 @@
-from odoo import api, fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
 
 class HallInvoice(models.Model):
     _name = 'hall.invoice'
@@ -19,6 +20,17 @@ class HallInvoice(models.Model):
     amount_tax = fields.Monetary(string='Tax Amount', compute='_compute_amounts', store=True)
     amount_total = fields.Monetary(string='Total', compute='_compute_amounts', store=True)
     currency_id = fields.Many2one('res.currency', string='Currency', default=lambda self: self.env.company.currency_id)
+
+    payment_state = fields.Selection([
+        ('not_paid', 'Not Paid'),
+        ('partial', 'Partially Paid'),
+        ('paid', 'Paid'),
+        ('reversed', 'Reversed'),
+    ], string='Payment Status', default='not_paid', tracking=True)
+
+    payment_ids = fields.Many2many('account.payment', string='Payments')
+    amount_paid = fields.Monetary(string='Amount Paid', compute='_compute_amount_paid')
+    amount_residual = fields.Monetary(string='Amount Due', compute='_compute_amount_paid')
 
     @api.model_create_multi
     def create(self, vals_list):
@@ -54,3 +66,32 @@ class HallInvoice(models.Model):
             invoice.amount_untaxed = sum(invoice.line_ids.mapped('price_subtotal'))
             invoice.amount_tax = sum(invoice.line_ids.mapped('price_tax'))
             invoice.amount_total = invoice.amount_untaxed + invoice.amount_tax 
+
+    @api.depends('payment_ids', 'amount_total')
+    def _compute_amount_paid(self):
+        for invoice in self:
+            amount_paid = sum(invoice.payment_ids.mapped('amount'))
+            invoice.amount_paid = amount_paid
+            invoice.amount_residual = invoice.amount_total - amount_paid
+            if amount_paid >= invoice.amount_total:
+                invoice.payment_state = 'paid'
+            elif amount_paid > 0:
+                invoice.payment_state = 'partial'
+            else:
+                invoice.payment_state = 'not_paid'
+
+    def action_register_payment(self):
+        ''' Open the account.payment.register wizard to pay the selected journal entries.
+        :return: An action opening the account.payment.register wizard.
+        '''
+        return {
+            'name': _('Register Payment'),
+            'res_model': 'hall.payment.register',
+            'view_mode': 'form',
+            'context': {
+                'active_model': 'hall.invoice',
+                'active_ids': self.ids,
+            },
+            'target': 'new',
+            'type': 'ir.actions.act_window',
+        }
